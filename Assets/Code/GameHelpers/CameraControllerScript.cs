503a3b11a15fc917d4dc43e5ad6a2aaa2c63d062
fr0stnk <nik7966@icloud.com>
2018-07-21 22:40:26 +0300

    Camera Fix

--- Assets/Code/GameHelpers/CameraControllerScript.cs
+++ Assets/Code/GameHelpers/CameraControllerScript.cs
@@ -23,7 +23,7 @@ public class CameraControllerScript : MonoBehaviour
             _audioSource.Play();
     }
 
-    void Update ()
+    void LateUpdate ()
     {
         if (!DoMovement)
         {
